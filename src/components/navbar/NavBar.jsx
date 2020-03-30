f75429dec68cd02c6e95dc1a2f8b07b63aa6e1a5
James Cornelius <jcorn2@umbc.edu>
2020-03-29 20:41:30 -0400

    Finish nav bar design for now

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/images/logo.png';
 
 function NavBar() {
     return (
-        <nav class="w-full h-32 flex justify-between items-center">
+        <nav class="w-full h-32 flex justify-between items-center border-b border-black">
             <img src={Logo} alt="logo" class="h-full" />
             <div class="flex flex-col h-full w-full justify-between">
                 <div class="bg-teal-600 flex justify-around items-center">
@@ -14,7 +14,7 @@ function NavBar() {
                     <button class="hover:bg-teal-400 flex-1 p-4 text-black font-bold">Donate</button>
                     <button class="hover:bg-teal-400 flex-1 p-4 text-black font-bold">Career</button>
                 </div>
-                <div class="flex justify-around items-center w-full">
+                <div class="flex justify-around items-center w-5/6 my-0 mx-auto">
                     <button class="text-black font-bold p-4 hover:bg-gray-200"n>Books &amp; More</button>
                     <button class="text-black font-bold p-4 hover:bg-gray-200">Digital Library</button>
                     <button class="text-black font-bold p-4 hover:bg-gray-200">Research &amp; Learn</button>
