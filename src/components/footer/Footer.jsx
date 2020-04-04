e3a3818e903fcb939d51c50b66a36adb9b8ce4d8
James Cornelius <jcorn2@umbc.edu>
2020-04-04 13:26:13 -0400

    Finished style of footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer className="w-full h-32 bg-teal-600 relative">
-            <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Anne Arundel County Public Library</p>
+        <footer className="w-full h-32 bg-teal-600 relative flex flex-col justify-center items-center">
+            <p className="w-1/6">Anne Arundel County Public Library</p>
         </footer>
     );
 }
